aa39276d22c1c2dd4e6746ac489e8d208ebe8726
heetop <wjdgmlxkr@gmail.com>
2020-05-30 09:39:45 +0900

    feat: parse image tags and click depending on time interval

--- src/js/content.js
+++ src/js/content.js
@@ -1,1 +1,13 @@
-console.log(`heetop`);
+// const imageUrls = document.querySelectorAll(`#islrg > div.islrc > div > a.wXeWr.islib.nfEiy.mM5pbd > div.bRMDJf.islir > img`);
+const imageTags = document.querySelectorAll(`#islrg > div.islrc > div > a.wXeWr.islib.nfEiy.mM5pbd`);
+
+function clickTag(index, interval = 5 * 1000) {
+    if (index >= imageTags.length || imageTags[index].tagName !== `A`) {
+        return;
+    }
+    
+    imageTags[index].click();
+    setTimeout(()=>clickTag(index+1), interval);
+}
+
+setTimeout(()=>clickTag(0), 5000);
