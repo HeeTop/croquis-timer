e5f63a8fe2e193747ab906b4ee3af39a925d50f5
heetop <wjdgmlxkr@gmail.com>
2020-05-31 15:46:59 +0900

    feat: layer style fixed and image poistion center

--- src/js/content.js
+++ src/js/content.js
@@ -1,19 +1,24 @@
 const imageAEls = document.querySelectorAll(`#islrg > div.islrc > div > a.wXeWr.islib.nfEiy.mM5pbd`);
-const mainImageEl = document.createElement("img");
-const layer = document.createElement("div");
+const mainImageEl = document.createElement(`img`);
+const layer = document.createElement(`div`);
 
-layer.id = "croquisTimerLayer";
-layer.style.position = "absolute";
-layer.style.top = "0";
-layer.style.left = "0";
-layer.style.width = "100%";
-layer.style.height = "100%";
-layer.style.zIndex = "1147483646";
-layer.style.backgroundColor = "green";
-layer.style.visibility = "visible";
+layer.id = `croquisTimerLayer`;
+layer.style.position = `absolute`;
+layer.style.top = `0`;
+layer.style.left = `0`;
+layer.style.width = `100%`;
+layer.style.height = `100%`;
+layer.style.zIndex = `1147483646`;
+layer.style.backgroundColor = `green`;
+layer.style.visibility = `visible`;
+layer.style.position = `fixed`;
+
+mainImageEl.style.display = `block`;
+mainImageEl.style.margin = `auto`;
+mainImageEl.style.width = `50%`;
 
 [...imageAEls].map(imageAEl=>{
-    imageAEl.addEventListener("click", ()=>{
+    imageAEl.addEventListener(`click`, ()=>{
         const previewEl = document.querySelector(`#Sva75c > div > div > div.pxAole > div.tvh9oe.BIB1wf > \
         c-wiz > div.OUZ5W > div.zjoqD > div > div.v4dQwb > a > img`);
         if (!previewEl) {
@@ -21,7 +26,6 @@ layer.style.visibility = "visible";
         }
 
         mainImageEl.src = previewEl.src;
-        setTimeout(()=>window.scrollTo(0,0),1000);
     });
 });
 
@@ -34,13 +38,14 @@ function clickTag(index, interval = 5 * 1000) {
     setTimeout(()=>clickTag(index+1), interval);
 }
 
-function init(on = true) {
+function init(on = true, interval = 5 * 1000) {
     if (!on) {
         return;
     }
-    setTimeout(()=>clickTag(0), 5);
+    setTimeout(()=>clickTag(0, interval), 5);
 
     document.body.appendChild(layer);
     layer.appendChild(mainImageEl);
 }
+
 init();
