eb003fd7a672fe52d788e83adf236a384434502d
heetop <wjdgmlxkr@gmail.com>
2022-01-02 22:48:08 +0900

    feat: set full screen when starting croquis timer

--- src/js/background.js
+++ src/js/background.js
@@ -1,4 +1,3 @@
-console.log(`HeeTop`);
 chrome.storage.local.set({status: false}, function() {
   console.log('init storage');
 });
@@ -16,4 +15,29 @@ chrome.browserAction.onClicked.addListener(function(tab) {
       console.log('switch status');
     });
   });
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  const statusChange = changes[`status`];
+  if (statusChange) {
+    if (statusChange.oldValue == statusChange.newValue) {
+      return
+    }
+    // 전체 화면 설정
+    chrome.windows.getCurrent(function(curWindow) {
+      // 크로키 시작
+      if (statusChange.newValue) {
+        // 전체 화면 여부 저장
+        chrome.storage.local.set({winStatus: curWindow.state});
+        chrome.windows.update(curWindow.id, { state: `fullscreen` });
+      } else {
+        chrome.storage.local.get(['winStatus'], function(result) {
+          if (result.length === 0) {
+            return;
+          }
+          chrome.windows.update(curWindow.id, { state: result[`winStatus`] });
+        });
+      }
+    });
+  }
 });
